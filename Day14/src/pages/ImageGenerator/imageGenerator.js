77b7b8c44b3b4712c51c42e5292d983d17ad86f1
Likhilesh <likbalpande@example.com>
2024-05-31 14:48:28 +0530

    image

--- Day14/src/pages/ImageGenerator/imageGenerator.js
+++ Day14/src/pages/ImageGenerator/imageGenerator.js
@@ -1,12 +1,23 @@
 import Navbar from "../common/Navbar/navbar";
 import "./imageGenerator.css"
+import {useState} from "react";
 
 const ImageGenerator = () => {
+    const [name, changeName] = useState("Aakash");
+
+    const func = (e) => {
+        changeName(e.target.value);
+    }
+
     return (
         <div>
             <Navbar />
             <div className="image-generator-main-container">
-                Image Page
+                <div className='image-search'>
+                    <img src="https://png.pngtree.com/thumb_back/fh260/background/20230817/pngtree-lotus-flower-jpg-pink-lotus-flower-image_13023952.jpg" />
+                    <input onChange={(e)=>{func(e)}}/>
+                </div>
+                {name}
             </div>
         </div>
     )
