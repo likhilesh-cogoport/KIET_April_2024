95e79b92cc15b8bb1f51cf7d457f5acf1ba69074
Likhilesh <likbalpande@example.com>
2024-05-31 10:50:53 +0530

    header file

--- Day14/app.js
+++ Day14/app.js
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Navbar from './src/pages/common/Navbar/navbar';
 
 const parent = document.getElementById("root");
 const root = ReactDOM.createRoot(parent);
 
-const Heading = <h1>Hello React</h1>
-
-const Title = ()=>{
-    return <h1>Hello React 3!</h1>;
+const App = ()=>{
+    return <Navbar />;
 }
 
-// root.render(Heading);
-// root.render(Title());
-root.render(<Title />);
-// <Title/> = Title()
+root.render(<App />);
---
+++ Day14/src/pages/common/Navbar/navbar.js
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return(
+        <div className='header-parent-container'>
+            <div className='left'>
+                Left
+            </div>
+            <div className="right">
+                right
+            </div>
+        </div>
+    )
+}
+
+export default Navbar;
