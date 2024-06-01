f534e17b79db0f89d7d7cc9e64be858917d80755
Likhilesh <likbalpande@example.com>
2024-06-01 14:02:08 +0530

    backend setup

--- KIET_MAJOR_BACKEND/app.js
+++ KIET_MAJOR_BACKEND/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const imageRouter = require('./routes/imageRouter.js');
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({origin: true}));
 app.use(express.json());
 app.use('/api/v1/images', imageRouter);
 
--- KIET_MAJOR_BACKEND/controllers/imageController.js
+++ KIET_MAJOR_BACKEND/controllers/imageController.js
@@ -1,6 +1,16 @@
 const generateImage = (req, res) => {
-    const body = req.body();
+    const body = req.body;
+    const searchText = body.searchText;
 
+    console.log("\n✅ : searchText:", req.body)
+
+
+    res.json({
+        status: 'success',
+        data: {
+            imageUrl: searchText,
+        }
+    })
 }
 
 module.exports = {
