7670bb978dbb8393eded63a0f16a532aca0e2a93
Likhilesh <likbalpande@example.com>
2024-06-01 10:57:39 +0530

    single item info page

--- Day14/src/pages/HistoryInformationPage/historyInformationPage.js
+++ Day14/src/pages/HistoryInformationPage/historyInformationPage.js
@@ -1,6 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
 const HistoryInformationPage = () => {
+    const [data, setData] = useState();
+
+    const params = useParams();
+    const historyId = params.historyId;
+
+    const customStyles = {
+        color: 'white',
+        padding: '48px',
+    }
+
+    const getData = async() => {
+        const res = await fetch(`https://dummyjson.com/products/${historyId}`);
+        const obj = await res.json();
+        setData(obj);
+    }
+
+
+
+    useEffect(() => {
+      getData();
+    }, []);
+    
+
     return (
-        <div> Info of: </div>
+        <div style={customStyles}> 
+            <h3>Info of: {historyId}</h3>
+            <img src={data?.thumbnail}/>
+            <img src={data?.images?.[0]}/>
+        </div>
     )
 }
 
