189c1591b5d4d7b9e8b8dd9cf3d555832765c934
Likhilesh <likbalpande@example.com>
2024-06-02 15:15:58 +0530

    setup env

--- KIET_MAJOR_BACKEND/controllers/imageController.js
+++ KIET_MAJOR_BACKEND/controllers/imageController.js
@@ -8,21 +8,21 @@ const generateImage = async (req, res) => {
     try{
         const res = await fetch("https://api.hotpot.ai/art-maker-sdte-zmjbcrr", {
             "headers": {
-                "accept": "*/*",
-                "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
-                "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTcyMzE3MTUsImV4cCI6MTcxNzMxODExNX0.sXShSsyEn88QsnvnYuLo-gySDniYmXBcjeeqkQOrJko",
-                "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
-                "content-type": "multipart/form-data; boundary=----WebKitFormBoundaryPLTzcTAVUoEpBM4s",
-                "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
-                "sec-ch-ua-mobile": "?0",
-                "sec-ch-ua-platform": "\"macOS\"",
-                "sec-fetch-dest": "empty",
-                "sec-fetch-mode": "cors",
-                "sec-fetch-site": "same-site"
+              "accept": "*/*",
+              "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+              "api-token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3MTczMjA2NjIsImV4cCI6MTcxNzQwNzA2Mn0.dxlUTFxMyb1UQleynZH57xuP-MYBv0PhWjDurbldq5o",
+              "authorization": "hotpot-t2mJbCr8292aQzp8CnEPaK",
+              "content-type": "multipart/form-data; boundary=----WebKitFormBoundarySc1jMvFKwB9bozjU",
+              "sec-ch-ua": "\"Chromium\";v=\"124\", \"Google Chrome\";v=\"124\", \"Not-A.Brand\";v=\"99\"",
+              "sec-ch-ua-mobile": "?0",
+              "sec-ch-ua-platform": "\"macOS\"",
+              "sec-fetch-dest": "empty",
+              "sec-fetch-mode": "cors",
+              "sec-fetch-site": "same-site"
             },
             "referrer": "https://hotpot.ai/",
             "referrerPolicy": "strict-origin-when-cross-origin",
-            "body": `------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-Kn3yizAWGfBEKhh\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-Kn3yizAWGfBEKhh.png\r\n------WebKitFormBoundaryPLTzcTAVUoEpBM4s--\r\n`,
+            "body": `------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"seedValue\"\r\n\r\nnull\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"inputText\"\r\n\r\n${searchText}\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"width\"\r\n\r\n512\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"height\"\r\n\r\n512\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"styleId\"\r\n\r\n49\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"styleLabel\"\r\n\r\nPhoto General 1\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"isPrivate\"\r\n\r\nfalse\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"price\"\r\n\r\n0\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"requestId\"\r\n\r\n8-QLsFfkOVZduOVEz\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU\r\nContent-Disposition: form-data; name=\"resultUrl\"\r\n\r\nhttps://hotpotmedia.s3.us-east-2.amazonaws.com/8-QLsFfkOVZduOVEz.png\r\n------WebKitFormBoundarySc1jMvFKwB9bozjU--\r\n`,
             "method": "POST",
             "mode": "cors",
             "credentials": "include"
--- KIET_MAJOR_BACKEND/server.js
+++ KIET_MAJOR_BACKEND/server.js
@@ -1,19 +1,17 @@
+require('dotenv').config()
 const mongoose = require('mongoose');
 const app = require('./app.js');
 
 const url = 'mongodb+srv://$_USERNAME_$:$_PASSWORD_$@cluster0.dpithd6.mongodb.net/$_DB_NAME_$?retryWrites=true&w=majority&appName=Cluster0';
-const databaseUser = 'likhileshexplorin';
-const databasePassword = 'abcd1234';
-const databaseName = 'KIET_MCA_AI_IMAGE';
 
-let dbLink = url.replace("$_USERNAME_$", databaseUser);
-dbLink = dbLink.replace("$_PASSWORD_$", databasePassword);
-dbLink = dbLink.replace("$_DB_NAME_$", databaseName);
+let dbLink = url.replace("$_USERNAME_$", process.env.DB_USER);
+dbLink = dbLink.replace("$_PASSWORD_$", process.env.DB_PASSWORD);
+dbLink = dbLink.replace("$_DB_NAME_$", process.env.DB_NAME);
 
 mongoose.connect(dbLink).then(() => {
   console.log('-------- Database Connected --------');
 });
 
-app.listen(1400,() => {
+app.listen(1600,() => {
     console.log('----------- App Started -----------')
 });
